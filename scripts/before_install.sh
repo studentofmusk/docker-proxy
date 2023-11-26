8d50ad432014a99cb5c2c781cf5f7023f947f540
mandalotech <134844048+mandalotech@users.noreply.github.com>
2023-11-26 20:49:05 +0530

    innoru dhadava

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose down
+docker-compose -f /var/www/html/docker-compose.yml down
 docker-compose -f /var/www/html/docker-compose.yml build
 docker-compose -f /var/www/html/docker-compose.yml pull
